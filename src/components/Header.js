e2f816e9874e4c1c884b683541c01bfe3c3f08f6
Matheus Simplicio <matheusschulz47@gmail.com>
2023-01-13 17:24:31 -0300

    req 1 & 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  state = {
+    total: 0,
+    currency: 'BRL',
+  };
+
   render() {
+    const { email } = this.props;
+    const { total, currency } = this.state;
     return (
-      <div>Header</div>
+      <div>
+        <p data-testid="email-field">
+          { email }
+        </p>
+        <span data-testid="total-field">
+          { total }
+        </span>
+        <span data-testid="header-currency-field">
+          { currency }
+        </span>
+      </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userEmailSaver } from '../redux/actions';
+
+const MAX_PASSWORD_SIZE = 6;
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  btnDisabled = () => {
+    const { email, password } = this.state;
+    return password.length < MAX_PASSWORD_SIZE
+    || !email.match(/\S+@\S+\.\S+/);
+  };
+
+  handleClick = (event) => {
+    event.preventDefault();
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+    dispatch(userEmailSaver(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form onSubmit={ this.handleClick }>
+        <input
+          type="email"
+          onChange={ this.handleChange }
+          name="email"
+          value={ email }
+          data-testid="email-input"
+          required
+        />
+        <input
+          type="password"
+          onChange={ this.handleChange }
+          name="password"
+          value={ password }
+          minLength="6"
+          data-testid="password-input"
+          required
+        />
+        <button type="submit" disabled={ this.btnDisabled() }>Entrar</button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
